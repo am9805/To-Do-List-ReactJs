cfb37e957f15ddd76cc1b3c517e066802c6e13d3
Angelica <angelica.arroyavem@udea.edu.co>
2018-09-19 18:29:34 -0500

    We literally copy and paste the form component, but it is the same as always, the render, the import of react component and more

--- src/App.js
+++ src/App.js
@@ -2,34 +2,65 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {todos} from './todos.json';
-console.log(todos);
+import { todos } from './todos.json';
+import TodoForm from './components/TodoForm';
+
 
 class App extends Component {
-  constructor(){
+  constructor() {
     //Super quiere decir que debe heredar toda
     //la funcionalidad del componente de react
     super();
     //Voy a crear el estado de este componente
     this.state = {
-      title: 'Aplicacion de tareas',
-      nTasks: 10
+      todos: todos
     }
   }
 
   render() {
+    //ESto es un arreglo
+    //Maps recorre uno a uno cada indice, me va a dar la tarea y el indice
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-hearder">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{todo.description}</p>
+              <p> <mark>{todo.responsable}</mark></p>
+            </div>
+          </div>
+        </div>
+      )
+    })
     return (
       <div className="App">
-          <img src={logo} className="App-logo" alt="logo" />
-          <nav className="navbar navbar-dark bg-dark">
-                <a href="" className="text-white">
-                    {//Dentro de estas llaves interpretamos Javascript
-                        this.state.title
-                    } - {
-                      this.state.nTasks
-                    }                
-                </a>
-            </nav>
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="" className="text-white">
+            tasks
+            <span className="badge badge-pill badge-light ml-2">
+              {this.state.todos.length}
+            </span>
+          </a>
+        </nav>
+        <div className="row mt-4">
+
+          <div className="col-md-4 text-center">
+            <img src={logo} className="App-logo" alt="logo" />
+            <TodoForm onAddTodo={this.handleAddTodo}></TodoForm>
+          </div>
+
+          <div className="col-md-8">
+            <div className="row">
+              {todos}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
