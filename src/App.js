763c1a37250d5f69881c13464f549f86480bbd2c
Angelica <angelica.arroyavem@udea.edu.co>
2018-09-19 16:25:06 -0500

    Know, we don't use the beautiful component that we created :c, but we lear abput the states and we add a burned data (?)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Navigation from './components/navigation'
+import {todos} from './todos.json';
+console.log(todos);
 
 class App extends Component {
+  constructor(){
+    //Super quiere decir que debe heredar toda
+    //la funcionalidad del componente de react
+    super();
+    //Voy a crear el estado de este componente
+    this.state = {
+      title: 'Aplicacion de tareas',
+      nTasks: 10
+    }
+  }
+
   render() {
     return (
       <div className="App">
           <img src={logo} className="App-logo" alt="logo" />
-          <Navigation></Navigation>
+          <nav className="navbar navbar-dark bg-dark">
+                <a href="" className="text-white">
+                    {//Dentro de estas llaves interpretamos Javascript
+                        this.state.title
+                    } - {
+                      this.state.nTasks
+                    }                
+                </a>
+            </nav>
       </div>
     );
   }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,11 +4,6 @@ class Navigation extends Component {
     //Todos los componentes tienen un método llamado render
     //porque es el que pinta las cosas en el navegardor, el html
     render() {
-        return (
-            <nav className="navbar navbar-dark bg-dark">
-                <a href="" className="text-white">Tareas</a>
-            </nav>
-        )
     }
 }
 
